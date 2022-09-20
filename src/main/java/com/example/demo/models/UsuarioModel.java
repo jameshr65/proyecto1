4debcb98a8325918dfa476e25bf70c805a2b5365
jameshr65 <jameshr65@hotmail.com>
2022-09-19 21:19:44 -0500

    solucion error

--- src/main/java/com/example/demo/models/UsuarioModel.java
+++ src/main/java/com/example/demo/models/UsuarioModel.java
@@ -9,7 +9,7 @@ public class UsuarioModel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(unique = true, nullable = false)
-	private Long cedula;
+	private Long id;
 	
 	@Column(name = "nombre")
 	private String nombre;
